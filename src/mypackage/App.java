e6a33d422b6ee72caf5a1224efe350e57b885ac8
Walid Amar <walidamarpro@gmail.com>
2022-09-19 19:22:13 -0400

    App Added

---
+++ src/mypackage/App.java
@@ -0,0 +1,21 @@
+package mypackage;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		IntBST mytree = new IntBST();
+		
+		mytree.insert(13);
+		mytree.insert(10);
+		mytree.insert(2);
+		mytree.insert(12);
+		mytree.insert(25);
+		mytree.insert(20);
+		mytree.insert(31);
+		mytree.insert(29);
+		
+		System.out.println(mytree.search(mytree.root, 12).key);
+	}
+
+}
